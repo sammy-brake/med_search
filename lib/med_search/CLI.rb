a2d6b8787d439a7995357f12312dc0482a5d3ce9
Samantha brake <sammy.brake@gmail.com>
2018-10-28 19:37:00 +0000

    updated info in cli.rb

--- lib/med_search/CLI.rb
+++ lib/med_search/CLI.rb
@@ -3,7 +3,18 @@
 class MedSearch::CLI
 
 def call
-puts "I'm working"
+puts "Welcome to Med Search!  I work!"
+drug_query
 end
+
+def send_info
+  #returns the drug info that the user is looking for
+  puts "Here is a description of the drug."
+end
+
+def drug_query
+  puts "Enter the name of the medication you are interested in learning about:"
+end
+
 
 end
