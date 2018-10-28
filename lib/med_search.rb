8dcd34abe89d0f1cb4aaa88c61b8beb303852b21
Samantha brake <sammy.brake@gmail.com>
2018-10-28 19:25:46 +0000

    updated file dependencies

--- lib/med_search.rb
+++ lib/med_search.rb
@@ -1,4 +1,5 @@
-require "med_search/version"
+require_relative "./med_search/version"
+require_relative './med_search/cli.rb'
 
 module MedSearch
   class Error < StandardError; end
--- lib/med_search/CLI.rb
+++ lib/med_search/CLI.rb
@@ -1,7 +1,9 @@
+#this controls the interface with the user
+
 class MedSearch::CLI
 
 def call
 puts "I'm working"
 end
 
-end 
+end
