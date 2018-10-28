000af82b8255801dc9995b5ec4b494f24e6db939
Samantha brake <sammy.brake@gmail.com>
2018-10-28 22:54:38 +0000

    scraped dosage

--- lib/med_search/scraper.rb
+++ lib/med_search/scraper.rb
@@ -6,7 +6,10 @@ class MedSearch::Scraper
     #this will eventually take the input and put it at the end of the URL we need to scrap
     html = open("https://www.goodrx.com/#{input}")
     doc = Nokogiri::HTML(html)
-
+    drug_url = "https://www.goodrx.com/#{input}"
+    dosage = doc.css('#uat-dropdown-dosage').text
+    quantity = doc.css()
+    puts dosage
     puts "You want to learn about #{input.capitalize}"
     puts "#{input.capitalize} is also known as #{doc.css('#uat-drug-alternatives a').text}"
 
