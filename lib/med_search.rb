aa7d3bbe115ba2e284f4a881fcd2061663d6169f
Samantha brake <sammy.brake@gmail.com>
2018-10-28 19:28:42 +0000

    fixed the file dependencies

--- lib/med_search.rb
+++ lib/med_search.rb
@@ -1,7 +1,2 @@
 require_relative "./med_search/version"
-require_relative './med_search/cli.rb'
-
-module MedSearch
-  class Error < StandardError; end
-  # Your code goes here...
-end
+require_relative './med_search/CLI.rb'
