9de1f3bc55640755ea259328e089747c05159045
Samantha brake <sammy.brake@gmail.com>
2018-10-28 19:43:49 +0000

    In this update, I wrote methods that can grap user iput

--- lib/med_search/CLI.rb
+++ lib/med_search/CLI.rb
@@ -1,19 +1,23 @@
 #this controls the interface with the user
-
+require 'pry'
 class MedSearch::CLI
 
 def call
 puts "Welcome to Med Search!  I work!"
-drug_query
-end
-
-def send_info
-  #returns the drug info that the user is looking for
-  puts "Here is a description of the drug."
+input = drug_query
+send_info(input)
 end
 
 def drug_query
   puts "Enter the name of the medication you are interested in learning about:"
+  input = gets.strip
+
+end
+
+def send_info(input)
+  #returns the drug info that the user is looking for
+  puts "Here is a description of the drug."
+  puts "You want to learn about #{input}"
 end
 
 
