9d76c8a0d9cbd5278585e5fdfdf9aa8a49647b75
Samantha brake <sammy.brake@gmail.com>
2018-10-28 22:16:52 +0000

    figured out open-uri and scraping!

--- lib/med_search.rb
+++ lib/med_search.rb
@@ -2,3 +2,6 @@ require_relative "./med_search/version"
 require_relative './med_search/CLI.rb'
 require_relative './med_search/scraper.rb'
 require_relative './med_search/drug.rb'
+
+require 'nokogiri'
+require 'open-uri'
