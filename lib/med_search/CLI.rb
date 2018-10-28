5477457223dc7187d6ca88ceeed40c39d3121813
Samantha brake <sammy.brake@gmail.com>
2018-10-28 20:34:55 +0000

    I have my cli loop written

--- lib/med_search/CLI.rb
+++ lib/med_search/CLI.rb
@@ -3,21 +3,39 @@ require 'pry'
 class MedSearch::CLI
 
 def call
-puts "Welcome to Med Search!  I work!"
+puts "I work!  Would you like to look up a medication?"
 input = drug_query
+if input == 'quit'
+  goodbye
+else
 send_info(input)
+second_call
+end
+end
+
+def second_call
+  input = drug_query
+  if input == 'quit'
+    goodbye
+  else
+  send_info(input)
+  second_call
+  end
 end
 
 def drug_query
-  puts "Enter the name of the medication you are interested in learning about:"
+  puts "Enter the name of the medication you are interested in learning about or enter quit to exit:"
   input = gets.strip
-
 end
 
 def send_info(input)
   #returns the drug info that the user is looking for
   puts "Here is a description of the drug."
   puts "You want to learn about #{input}"
+end
+
+def goodbye
+  puts "Goodbye!"
 end
 
 
