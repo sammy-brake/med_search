6172a905cfd50f042ee4cdae6bdfaf8d3289810b
Samantha brake <sammy.brake@gmail.com>
2018-10-28 21:19:20 +0000

    added drug.rb class

--- lib/med_search.rb
+++ lib/med_search.rb
@@ -1,3 +1,4 @@
 require_relative "./med_search/version"
 require_relative './med_search/CLI.rb'
 require_relative './med_search/scraper.rb'
+require_relative './med_search/drug.rb'
---
+++ lib/med_search/drug.rb
@@ -0,0 +1,6 @@
+class MedSearch::Drug
+  attr_accessor :url, :description, :dosage, :quantity
+
+  def initialize(attributes)
+  end
+end
